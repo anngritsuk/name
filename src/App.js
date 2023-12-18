b604dc95271fbde0f176938ee3676dc3c38aba73
anngritsuk <annagritsuk04@gmail.com>
2023-12-19 02:11:29 +0300

    add my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lastName: "",
+      firstName: "",
+      patronymicName: ""
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { lastName, firstName, patronymicName } = this.state;
+
+    return (
+      <div className="container">
+        <div className="wrapper">
+          <div>
+            <input
+              type="text"
+              className="input"
+              name="lastName"
+              onChange={this.handleChange}
+              value={lastName}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              className="input"
+              name="firstName"
+              onChange={this.handleChange}
+              value={firstName}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              className="input"
+              name="patronymicName"
+              onChange={this.handleChange}
+              value={patronymicName}
+            />
+          </div>
+        </div>
+        <div className="output">
+          <div>{lastName} </div>
+          <div>{firstName} </div>
+          <div>{patronymicName}</div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
